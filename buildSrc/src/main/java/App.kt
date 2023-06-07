a1a57c80ab911c89c39752ab7af3cc53f45ef956
sinaks <sinaks333@gmail.com>
2023-06-08 00:26:56 +0300

    Version inc 1.0.13(26);

--- buildSrc/src/main/java/App.kt
+++ buildSrc/src/main/java/App.kt
@@ -11,9 +11,9 @@ object App {
     val name
         get() = "$major.$minor.$patch"
 
-    private var version = 25
+    private var version = 26
 
     private var major = 1
     private var minor = 0
-    private var patch = 12
+    private var patch = 13
 }
