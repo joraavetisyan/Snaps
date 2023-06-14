eb17656464c6de7160fdc967e1ca2a263d968d2a
dinaibragimova <DinaIbragimova>
2023-06-15 00:16:06 +0300

    Version inc 1.0.18(31)

--- buildSrc/src/main/java/App.kt
+++ buildSrc/src/main/java/App.kt
@@ -11,9 +11,9 @@ object App {
     val name
         get() = "$major.$minor.$patch"
 
-    private var version = 30
+    private var version = 31
 
     private var major = 1
     private var minor = 0
-    private var patch = 17
+    private var patch = 18
 }
