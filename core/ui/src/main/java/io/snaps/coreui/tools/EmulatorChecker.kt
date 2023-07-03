a770c4cd66e37efdf860a0a6bcb7f62410d92ab7
Dmitrii <actovus@gmail.com>
2023-07-03 20:13:06 +0300

    SNAPS-322 bluestacks emulator fixed

--- core/ui/src/main/java/io/snaps/coreui/tools/EmulatorChecker.kt
+++ core/ui/src/main/java/io/snaps/coreui/tools/EmulatorChecker.kt
@@ -19,6 +19,17 @@ object EmulatorChecker {
                 || Build.PRODUCT.contains("sdk_x86")
                 || Build.PRODUCT.contains("vbox86p")
                 || Build.PRODUCT.contains("emulator")
-                || Build.PRODUCT.contains("simulator")
+                || Build.PRODUCT.contains("simulator") ||
+                Build.MODEL.contains("VirtualBox") ||
+                ("QC_Reference_Phone" == Build.BOARD && !"Xiaomi".equals(Build.MANUFACTURER, ignoreCase = true)) ||
+                Build.HOST == "Build2" ||
+                Build.PRODUCT == "google_sdk" ||
+                System.getProperties().getProperty("ro.kernel.qemu") == "1" ||
+                Build.FINGERPRINT.startsWith("google/sdk_gphone") &&
+                Build.FINGERPRINT.endsWith(":user/release-keys") &&
+                Build.MANUFACTURER == "Google" &&
+                Build.PRODUCT.startsWith("sdk_gphone") &&
+                Build.BRAND == "google" &&
+                Build.MODEL.startsWith("sdk_gphone")
     }
 }
