7158df8f808571051b02246b31e92a4a79689940
Zhora Avetisyan <joraavetisyan@gmail.com>
2023-07-04 21:10:47 +0400

    Version inc 1.0.27(40)

--- buildSrc/src/main/java/App.kt
+++ buildSrc/src/main/java/App.kt
@@ -11,9 +11,9 @@ object App {
     val name
         get() = "$major.$minor.$patch"
 
-    private var version = 39
+    private var version = 40
 
     private var major = 1
     private var minor = 0
-    private var patch = 26
+    private var patch = 27
 }
